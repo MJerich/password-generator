de534e94539ed003d60d56259376e65b38132f8b
MJerich <MatthewJerich@gmail.com>
2022-02-19 15:48:46 -0700

    finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
 // Assignment code here
+// assign character set variables
+const lowerCaseSet = 'abcdefghijklmnopqrstuvwxyz'
+const upperCaseSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const numberCharsSet = '0123456789'
+const specialCharsSet = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
 // function to check for a valid length
 function validLength(x) {
   return x >= 8 && x <= 128;
@@ -8,6 +13,8 @@ function validLength(x) {
 function generatePassword() {
   // find the desired password length
   let passwordLength = window.prompt('Your password can be 8-128 characters long. How long would you like it to be?')
+
+  charSet = ""
   
   // turn the password lenght into and integer and round to the nearest whole number
   passwordLength = parseInt(Math.round(passwordLength));
@@ -18,19 +25,44 @@ function generatePassword() {
     let upperCase = window.confirm('Do you want UPPERCASE characters?');
     let numberChars = window.confirm('Do you want numbers?');
     let specialChars = window.confirm('Do you want special characters?');
-    if (!lowerCase && !upperCase && !numberChars && !specialChars) {
-      console.log('all false')
-    } else {
-      console.log('NOT all false')
-    }
-  } else {
-    window.alert("that is not a valid number");
-    generatePassword();
-  }
 
-  let secretCode = 'sUpeRsEcReTpAsSwOrD'
-  return secretCode
-}
+    // if staements to check what types the user picked
+    if (lowerCase) {
+      charSet += lowerCaseSet
+    };
+    if (upperCase) {
+      charSet += upperCaseSet
+    };
+    if (numberChars) {
+      charSet += numberCharsSet
+    };
+    if (specialChars) {
+      charSet += specialCharsSet
+    };
+    if (charSet === "") {
+      window.alert("You must choose atleast one chracter type")
+      generatePassword();
+    };
+    // generate the password with users selections
+    var length = passwordLength,
+        finalPassword = "";
+    for (var i = 0, n = charSet.length; i < length; ++i) {
+      finalPassword += charSet.charAt(Math.floor(Math.random() * n));
+    }
+    return finalPassword;
+  } 
+  else {
+      window.alert("that is not a number.");
+      generatePassword();
+  }
+  // generate the password with users selections
+  var length = passwordLength,
+      finalPassword = "";
+  for (var i = 0, n = charSet.length; i < length; ++i) {
+    finalPassword += charSet.charAt(Math.floor(Math.random() * n));
+  }
+  return finalPassword;
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
